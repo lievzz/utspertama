582fa76b8a4385bf28247f8037b438e009306fcb
lievzz <muhammadtegar019@gmail.com>
2023-05-11 18:07:19 +0700

    UTS

--- pengguna/User.java
+++ pengguna/User.java
@@ -1,5 +1,28 @@
 package pengguna;
 
-public class User {
+public class User{
+    private String username;
+    private String password;
+
+    public User(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+    
+    public void setUsername(String username){
+        this.username = username;
+    }
+    
+    public void setPassword(String password){
+        this.password = password;
+    }
+    
+    public String getUsername(){
+        return this.username = username;
+    }
+    
+    public String getPassword() {
+        return this.password = password;
+  }
 
 }
